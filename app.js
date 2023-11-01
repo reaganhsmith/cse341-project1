9e3dcc5a626e13eec72f51f3ee87cdb23bd0c954
reaganhsmith <reagansmith@byui.edu>
2023-11-01 15:23:42 -0600

    connecting to MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -6,6 +8,14 @@ const port = process.env.PORT || 3000;
 app.use('/', require('./routes'));
 
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}!`)
-});
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Database is listening and running on port ${port}!`)
+        });
+    }
+})
+
+
